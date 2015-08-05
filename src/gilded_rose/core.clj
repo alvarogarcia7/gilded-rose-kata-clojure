d9be204de9d682401cfcfaf40f6aa92804e3e7db
alvaro garcia <derecerca@gmail.com>
2015-08-05 22:01:39 +0200

    once the item has expired, the quality degrades twice as fast

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -7,10 +7,15 @@
 (def non-expired-item (item 1 2))
 
 (defn age-one-day [it]
-  (item (dec (:sellin it))
-        (dec (:quality it))))
+  (if (< (:sellin it) 0)
+    (item (dec (:sellin it))
+        (dec (dec (:quality it))))
+    (item (dec (:sellin it))
+          (dec (:quality it)))))
 
 (age-one-day (age-one-day (item 1 2)))
+(age-one-day (item 1 2))
+
 
 (:sellin non-expired-item)
 (:quality non-expired-item)
