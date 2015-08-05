3f42dae5d6267501a766c8f15e40b964550000c5
alvaro garcia <derecerca@gmail.com>
2015-08-05 23:10:06 +0200

    add backstage passes
    'Backstage passes', like aged brie, increases in Quality as it's SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -7,6 +7,11 @@
 
 (def non-expired-item (item "normal" 1 2))
 
+(defn between [lower-bound x upper-bound]
+  (and
+    (<= lower-bound x)
+    (<= x upper-bound)))
+
 (defn age-one-day [it]
   (if (< (:sellin it) 0)
     (item (:name it)
@@ -15,14 +20,21 @@
             (min (inc (inc (:quality it))) 50)
             (if (= "Sulfuras" (:name it))
               (:quality it)
-              (max (dec (dec (:quality it))) 0))))
+              (if (= "Backstage passes" (:name it))
+                0
+                (max (dec (dec (:quality it))) 0)))))
     (item (:name it)
           (dec (:sellin it))
           (if (= "Aged Brie" (:name it))
             (min (inc (:quality it)) 50)
             (if (= "Sulfuras" (:name it))
               (:quality it)
-              (max (dec (:quality it)) 0))))))
+              (if (= "Backstage passes" (:name it))
+                (cond
+                  (between 7 (:sellin it) 11) (inc (inc (:quality it)))
+                  (between 0 (:sellin it) 6) (inc (inc (inc (:quality it))))
+                  :else 0)
+                (max (dec (:quality it)) 0)))))))
 
 (age-one-day (age-one-day (item "a" 1 2)))
 (age-one-day (item "a" 1 2))
@@ -30,6 +42,7 @@
 (age-one-day (age-one-day (age-one-day (item "Aged Brie" 1 0))))
 (age-one-day (item "Aged Brie" 10 50))
 (age-one-day (item "Sulfuras" -1 1))
+(age-one-day (age-one-day (item "Backstage passes" 0 10)))
 
 (:sellin non-expired-item)
 (:quality non-expired-item)
